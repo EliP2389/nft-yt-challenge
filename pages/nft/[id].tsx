53c764500fd0b11d58b21a90faff127100898736
Eli Pagan <mbk8689@gmail.com>
2022-04-28 20:02:17 -0400

    added divs for main page seperation

--- pages/nft/[id].tsx
+++ pages/nft/[id].tsx
@@ -1,6 +1,19 @@
+import React from 'react'
+
 function NFTDropPage() {
   return (
-    <div>NFTDropPage</div>
+    <div>
+      {/*left*/}
+      <div className='bg-gradient-to-br from-cyan-800 to-rose-500'>
+        <div>
+
+        </div>
+      </div>
+      {/* {right} */}
+      <div>
+
+      </div>
+    </div>
   )
 }
-export default NFTDropPage;
+export default NFTDropPage
