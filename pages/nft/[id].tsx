8f4046fda17cf6c1da23ea153ec1e0e0ef14a9a1
Eli Pagan <mbk8689@gmail.com>
2022-04-28 20:30:29 -0400

    added padding and rounded border to the div surrounding the img

--- pages/nft/[id].tsx
+++ pages/nft/[id].tsx
@@ -4,9 +4,9 @@ function NFTDropPage() {
   return (
     <div className="flex h-screen flex-col">
       {/*left*/}
-      <div className="bg-gradient-to-br from-cyan-800 to-rose-500">
+      <div className="bg-gradient-to-br from-yellow-500 to-purple-500">
         <div className="flex flex-col items-center justify-center py-2 lg:min-h-screen">
-          <div className="bg-gradient-to-br from-yellow-500 to-purple-500">
+          <div className="bg-gradient-to-br from-rose-500 to-blue-400 p-2 rounded-xl">
             <img
               className="w-44 rounded-xl object-cover lg:h-96 lg:w-72"
               src="https://pbs.twimg.com/media/E_gu7t9WEAwJKzr?format=jpg&name=4096x4096"
