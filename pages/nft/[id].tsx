2ad68ecfded5192d99756896412588bed738ec32
Eli Pagan <mbk8689@gmail.com>
2022-04-28 20:07:31 -0400

    added nft ape image

--- pages/nft/[id].tsx
+++ pages/nft/[id].tsx
@@ -2,11 +2,12 @@ import React from 'react'
 
 function NFTDropPage() {
   return (
-    <div>
+    <div className="flex h-screen flex-col">
       {/*left*/}
       <div className='bg-gradient-to-br from-cyan-800 to-rose-500'>
         <div>
-
+<img src='https://pbs.twimg.com/media/E_gu7t9WEAwJKzr?format=jpg&name=4096x4096' 
+alt="nft ape" />
         </div>
       </div>
       {/* {right} */}
