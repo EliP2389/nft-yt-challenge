ed26fce5a4c48e2d06e946836e22da79d5245434
Eli Pagan <mbk8689@gmail.com>
2022-04-28 20:13:46 -0400

    styled mft image

--- pages/nft/[id].tsx
+++ pages/nft/[id].tsx
@@ -4,16 +4,25 @@ function NFTDropPage() {
   return (
     <div className="flex h-screen flex-col">
       {/*left*/}
-      <div className='bg-gradient-to-br from-cyan-800 to-rose-500'>
+      <div className="bg-gradient-to-br from-cyan-800 to-rose-500">
         <div>
-<img src='https://pbs.twimg.com/media/E_gu7t9WEAwJKzr?format=jpg&name=4096x4096' 
-alt="nft ape" />
+          <img
+            className="w-44 rounded-xl object-cover lg:h-96 lg:w-72"
+            src="https://pbs.twimg.com/media/E_gu7t9WEAwJKzr?format=jpg&name=4096x4096"
+            alt="nft ape"
+          />
+          <div>
+              <h1>
+                  SLAYFEST APES
+              </h1>
+              <h2>
+                  A collection of SLAYFEST APES killing it using REACT!
+              </h2>
+          </div>
         </div>
       </div>
       {/* {right} */}
-      <div>
-
-      </div>
+      <div></div>
     </div>
   )
 }
