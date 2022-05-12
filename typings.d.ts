6d4f720410a0f782cc8ba9f518a2f599af2a39c4
Eli Pagan <mbk8689@gmail.com>
2022-05-12 08:24:43 -0400

    added interface image and creator to typings file

--- typings.d.ts
+++ typings.d.ts
@@ -1,5 +1,18 @@
+import { implementsInterface } from "@thirdweb-dev/sdk"
+
 interface Image {
     asset: {
         url: string
     }
+}
+
+export interface Creator {
+    _id,
+  name,
+  address,
+  slug: {
+  current: string
+}
+  image: image
+  bio: string
 }
